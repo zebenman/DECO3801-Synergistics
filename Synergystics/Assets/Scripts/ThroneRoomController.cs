7d6a28807c36598af8a590f8f6d1b5e34e615ddf
Lachlan Frawley <lachfrawley@gmail.com>
2020-10-27 21:14:22 +1000

    Deactivate Map

--- Synergystics/Assets/Scripts/ThroneRoomController.cs
+++ Synergystics/Assets/Scripts/ThroneRoomController.cs
@@ -10,15 +10,23 @@ public class ThroneRoomController : MonoBehaviour
     public GameObject LocationWidget;
     public GameObject BackgroundImage;
     public GameObject AdvisorText;
+    public GameObject MapText;
 
     public void Start()
     {
         CouncilRoomWidget.GetComponent<Button>().enabled = GameController.Instance.HasSelectedFocus;
+        MapWidget.GetComponent<Button>().enabled = !GameController.Instance.HasSelectedFocus;
+
         AdvisorText.SetActive(!GameController.Instance.HasSelectedFocus);
-        if(!GameController.Instance.HasSelectedFocus)
+        MapText.SetActive(GameController.Instance.HasSelectedFocus);
+
+        float cc = 65 / 255f;
+        if (!GameController.Instance.HasSelectedFocus)
         {
-            float cc = 65f / 255f;
             Utilities.SetWidgetColorRecursive(CouncilRoomWidget, new Color(cc, cc, cc), AdvisorText);
+        } else
+        {
+            Utilities.SetWidgetColorRecursive(MapWidget, new Color(cc, cc, cc), MapText);
         }
     }
 }

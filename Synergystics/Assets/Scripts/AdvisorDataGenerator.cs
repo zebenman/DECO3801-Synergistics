18d68aa009f0426694471606a65c1f01805ea96c
Lachlan Frawley <lachfrawley@gmail.com>
2020-10-28 16:33:51 +1000

    Maybe fixed?

--- Synergystics/Assets/Scripts/AdvisorDataGenerator.cs
+++ Synergystics/Assets/Scripts/AdvisorDataGenerator.cs
@@ -3,14 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "AdvisorPicture", menuName = "Scriptable/AdvisorPicture", order = 0)]
-public class AdvisorPicture : ScriptableObject
-{
-    public Sprite AdvisorImage;
-    public AdvisorType AdvisorType;
-    public AdvisorGender Gender;
-}
-
 public class AdvisorDataGenerator
 {
     private List<string> FemaleNames = new List<string>();
---
+++ Synergystics/Assets/Scripts/AdvisorPicture.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "AdvisorPicture", menuName = "Scriptable/AdvisorPicture", order = 0)]
+public class AdvisorPicture : ScriptableObject
+{
+    public Sprite AdvisorImage;
+    public AdvisorType AdvisorType;
+    public AdvisorGender Gender;
+}
+

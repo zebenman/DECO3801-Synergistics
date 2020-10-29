e7664e966997b0514581c71264e522172b1ea4c6
Lachlan Frawley <lachfrawley@gmail.com>
2020-10-29 17:04:30 +1000

    Fixed bug

--- Synergystics/Assets/Scripts/ButtonScript.cs
+++ Synergystics/Assets/Scripts/ButtonScript.cs
@@ -10,15 +10,18 @@ public class ButtonScript : MonoBehaviour
     public void Btn_change_scene(string scene_name)
     {
         string currentScene = SceneManager.GetActiveScene().name;
-        if(GameController.Instance.StoryManager.PeekNextThread() == null && scene_name.Equals(SceneInformation.INTERMISSION_SCREEN))
+        if(GameController.Instance != null)
         {
-            // Go to the final summary screen if we have no more story to do
-            scene_name = SceneInformation.FINAL_SUMMARY;
-        }
-        if(currentScene.Equals(SceneInformation.FINAL_SUMMARY))
-        {
-            // If we're on the final summary screen, go to the main menu instead of whereve we were going
-            scene_name = SceneInformation.MAIN_MENU;
+            if (GameController.Instance.StoryManager.PeekNextThread() == null && scene_name.Equals(SceneInformation.INTERMISSION_SCREEN))
+            {
+                // Go to the final summary screen if we have no more story to do
+                scene_name = SceneInformation.FINAL_SUMMARY;
+            }
+            if (currentScene.Equals(SceneInformation.FINAL_SUMMARY))
+            {
+                // If we're on the final summary screen, go to the main menu instead of whereve we were going
+                scene_name = SceneInformation.MAIN_MENU;
+            }
         }
 
         SceneManager.LoadSceneAsync(scene_name).completed += (a) =>
